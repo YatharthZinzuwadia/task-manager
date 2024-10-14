8d07fa2699248f3ede77446b49c6c11556b42bc6
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-10-15 00:28:21 +0530

    TaskList Component rectified of errors and props defined and passed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import TaskItem from "./TaskItem";
+import { Task } from "../types";
 
-const TaskList = () => {
+interface TaskListProps {
+  tasks: Task[]; // Array of tasks
+  toggleComplete: (id: number) => void; // Function to toggle task completion
+  deleteTask: (id: number) => void; // Function to delete a task
+}
+
+const TaskList: React.FC<TaskListProps> = ({
+  tasks,
+  toggleComplete,
+  deleteTask,
+}) => {
   return (
     <div>
-      <TaskItem />
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          toggleComplete={toggleComplete}
+          deleteTask={deleteTask}
+        />
+      ))}
     </div>
   );
 };
