5f4f7cdd95238028d075f2700b1d4116d9b4dd2d
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-10-15 10:17:50 +0530

    testing: added queries for input and button element

--- src/tests/AddTaskForm.test.tsx
+++ src/tests/AddTaskForm.test.tsx
@@ -6,4 +6,10 @@ describe("AddTaskForm", () => {
     // it("should add a new task", () => {
     //     const addTask = jest.fn();
     // })
+    test("render form correctly", ()=>{
+        //render component and store in vDOM
+        render(<AddTaskForm addTask={jest.fn()}/>);//passing mock props
+        const inputTask = screen.getByPlaceholderText("Add New Task"); //query for input element
+        const buttonElement = screen.getByRole("button"); //query for button element
+    })
 })
