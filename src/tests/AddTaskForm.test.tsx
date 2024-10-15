414670a0311dc70e2cabc1b530b15a856cb2d047
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-10-15 11:30:14 +0530

    added test cases and rectified errors for AddTaskForm. all test cases passed

--- src/tests/AddTaskForm.test.tsx
+++ src/tests/AddTaskForm.test.tsx
@@ -9,11 +9,11 @@ describe("AddTaskForm", () => {
   test("render form correctly", () => {
     //render component and store in vDOM
     render(<AddTaskForm addTask={jest.fn()} />); //passing mock props
-    const inputElement = screen.getByPlaceholderText("Add New Task"); //query for input element
+    const inputElement = screen.getByLabelText("New Task"); //query for input element
     const buttonElement = screen.getByRole("button"); //query for button element
 
-    expect(inputElement).toBeInDocument();
-    expect(buttonElement).toBeInDocument();
+    expect(inputElement).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
   });
 
   //Test case 1: check if addTask is called onSubmit
@@ -21,7 +21,22 @@ describe("AddTaskForm", () => {
     const addTaskMock = jest.fn(); //mock fn to track call
     render(<AddTaskForm addTask={addTaskMock} />); //render component with mock fn
 
-    const inputElement = screen.getByPlaceholderText("Add New Task");
+    const inputElement = screen.getByLabelText("New Task");
     const buttonElement = screen.getByRole("button");
+
+    fireEvent.change(inputElement, {
+      target: {
+        value: "NewTask",
+      },
+    });
+
+    fireEvent.click(buttonElement);
+
+    expect(addTaskMock).toHaveBeenCalledWith({
+      //   id: expect.toSatisfy((id) => typeof id === "number"),
+      id: expect.any(Number),
+      text: "NewTask",
+      completed: false,
+    });
   });
 });
