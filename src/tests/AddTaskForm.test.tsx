149449eee2a4c474e1b37344c4290544a763e326
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-10-15 10:24:55 +0530

    defining the test case to check for addTask call onSubmit

--- src/tests/AddTaskForm.test.tsx
+++ src/tests/AddTaskForm.test.tsx
@@ -3,13 +3,25 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import AddTaskForm from "../components/AddTaskForm";
 
 describe("AddTaskForm", () => {
-    // it("should add a new task", () => {
-    //     const addTask = jest.fn();
-    // })
-    test("render form correctly", ()=>{
-        //render component and store in vDOM
-        render(<AddTaskForm addTask={jest.fn()}/>);//passing mock props
-        const inputTask = screen.getByPlaceholderText("Add New Task"); //query for input element
-        const buttonElement = screen.getByRole("button"); //query for button element
-    })
-})
+  // it("should add a new task", () => {
+  //     const addTask = jest.fn();
+  // })
+  test("render form correctly", () => {
+    //render component and store in vDOM
+    render(<AddTaskForm addTask={jest.fn()} />); //passing mock props
+    const inputElement = screen.getByPlaceholderText("Add New Task"); //query for input element
+    const buttonElement = screen.getByRole("button"); //query for button element
+
+    expect(inputElement).toBeInDocument();
+    expect(buttonElement).toBeInDocument();
+  });
+
+  //Test case 1: check if addTask is called onSubmit
+  test("check if addTask is called onSubmit", () => {
+    const addTaskMock = jest.fn(); //mock fn to track call
+    render(<AddTaskForm addTask={addTaskMock} />); //render component with mock fn
+
+    const inputElement = screen.getByPlaceholderText("Add New Task");
+    const buttonElement = screen.getByRole("button");
+  });
+});
